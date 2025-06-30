134980acc75a6214527d1ef91175bcde03a9256f
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-06-30 09:48:31 +0700

    add export to pdf

---
+++ export-data/src/main/java/com/bintang/ExportDataApplication.java
@@ -0,0 +1,14 @@
+package com.bintang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportDataApplication.class, args);
+		System.out.println("\nSERVER IS RUNNING");
+	}
+
+}
