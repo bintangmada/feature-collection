5ea00e7753e4cca6cdd0fa74920850c0cd09fa8b
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-07-25 08:49:58 +0700

    add email-confirmation feature

---
+++ email-confirmation/src/test/java/com/bintang/email_confirmation/EmailConfirmationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bintang.email_confirmation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailConfirmationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
